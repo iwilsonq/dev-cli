4b40842ac45a6cc5e30809b42945192804cce4de
Ian Wilson <iwilsonq@gmail.com>
2019-12-01 18:26:56 -0800

    after asyncx

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,45 @@
 use std::env;
 use std::io::{self, Write};
+use tokio;
 
-use hyper::rt::{self, Future, Stream};
 use hyper::Client;
 
-fn fetch_url(url: hyper::Uri) -> impl Future<Item = (), Error = ()> {
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+
+async fn fetch_url(url: hyper::Uri) -> Result<()> {
     let client = Client::new();
 
-    client
-        // Fetch the url...
-        .get(url)
-        // And then, if we get a response back...
-        .and_then(|res| {
-            println!("Response: {}", res.status());
-            println!("Headers: {:#?}", res.headers());
+    let res = client.get(url).await?;
 
-            // The body is a stream, and for_each returns a new Future
-            // when the stream is finished, and calls the closure on
-            // each chunk of the body...
-            res.into_body().for_each(|chunk| {
-                io::stdout()
-                    .write_all(&chunk)
-                    .map_err(|e| panic!("example expects stdout is open, error={}", e))
-            })
-        })
-        // If all good, just tell the user...
-        .map(|_| {
-            println!("\n\nDone.");
-        })
-        // If there was an error, let the user know...
-        .map_err(|err| {
-            eprintln!("Error {}", err);
-        })
+    println!("Response: {}", res.status());
+    println!("Headers: {:#?}\n", res.headers());
+    let mut body = res.into_body();
+
+    while let Some(next) = body.next().await {
+        let chunk = next?;
+        io::stdout().write_all(&chunk)?;
+    }
+
+    println!("\n\nDone!");
+
+    Ok(())
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()> {
     let url = match env::args().nth(1) {
         Some(url) => url,
         None => {
             println!("Usage: client <url>");
-            return;
+            return Ok(());
         }
     };
 
     let url = url.parse::<hyper::Uri>().unwrap();
     if url.scheme_part().map(|s| s.as_ref()) != Some("http") {
         println!("This example only works with 'http' URLs.");
-        return;
+        return Ok(());
     }
 
-    rt::run(fetch_url(url));
+    fetch_url(url).await
 }
